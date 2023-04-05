1be4ea9ae1fcd3068e3aa485577a10eb9411b3bf
AldanaNonnenmacher <89547316+AldanaNonnenmacher@users.noreply.github.com>
2023-04-05 10:36:05 -0300

    Se agrega release date a SongDescriptionHelperImpl y se borra la propiedad year, ademas se agrega a SpotifyTableConstants release date precision

--- app/src/main/java/ayds/winchester/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -18,7 +18,7 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Year: ${song.year}"
+                        "Release date: ${song.releaseFormateado}"
             else -> "Song not found"
         }
     }
