9d3d677e857471b8ac008b62f056ffff4640b8b0
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2023-04-14 16:51:34 -0300

    Conversion de Java a Kotlin
    Se cambio el archivo .java a .kt

---
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -0,0 +1,116 @@
+package ayds.winchester.songinfo.moredetails.fulllogic
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ayds.winchester.songinfo.R
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.squareup.picasso.Picasso
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.io.IOException
+import java.util.*
+
+class OtherInfoWindow : AppCompatActivity() {
+    private var textPane2: TextView? = null
+
+    //private JPanel imagePanel;
+    // private JLabel posterImageLabel;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_info)
+        textPane2 = findViewById(R.id.textPane2)
+        open(intent.getStringExtra("artistName"))
+    }
+
+    fun getARtistInfo(artistName: String?) {
+
+        // create
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://en.wikipedia.org/w/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        val wikipediaAPI = retrofit.create(WikipediaAPI::class.java)
+        Log.e("TAG", "artistName $artistName")
+        Thread {
+            var text = DataBase.getInfo(dataBase, artistName)
+            if (text != null) { // exists in db
+                text = "[*]$text"
+            } else { // get from service
+                val callResponse: Response<String>
+                try {
+                    callResponse = wikipediaAPI.getArtistInfo(artistName).execute()
+                    println("JSON " + callResponse.body())
+                    val gson = Gson()
+                    val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
+                    val query = jobj["query"].asJsonObject
+                    val snippet = query["search"].asJsonArray[0].asJsonObject["snippet"]
+                    val pageid = query["search"].asJsonArray[0].asJsonObject["pageid"]
+                    if (snippet == null) {
+                        text = "No Results"
+                    } else {
+                        text = snippet.asString.replace("\\n", "\n")
+                        text = textToHtml(text, artistName)
+
+
+                        // save to DB  <o/
+                        DataBase.saveArtist(dataBase, artistName, text)
+                    }
+                    val urlString = "https://en.wikipedia.org/?curid=$pageid"
+                    findViewById<View>(R.id.openUrlButton).setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(urlString)
+                        startActivity(intent)
+                    }
+                } catch (e1: IOException) {
+                    Log.e("TAG", "Error $e1")
+                    e1.printStackTrace()
+                }
+            }
+            val imageUrl =
+                "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
+            Log.e("TAG", "Get Image from $imageUrl")
+            val finalText = text
+            runOnUiThread {
+                Picasso.get().load(imageUrl).into(findViewById<View>(R.id.imageView) as ImageView)
+                textPane2!!.text = Html.fromHtml(finalText)
+            }
+        }.start()
+    }
+
+    private var dataBase: DataBase? = null
+    private fun open(artist: String?) {
+        dataBase = DataBase(this)
+        DataBase.saveArtist(dataBase, "test", "sarasa")
+        Log.e("TAG", "" + DataBase.getInfo(dataBase, "test"))
+        Log.e("TAG", "" + DataBase.getInfo(dataBase, "nada"))
+        getARtistInfo(artist)
+    }
+
+    companion object {
+        const val ARTIST_NAME_EXTRA = "artistName"
+        fun textToHtml(text: String, term: String?): String {
+            val builder = StringBuilder()
+            builder.append("<html><div width=400>")
+            builder.append("<font face=\"arial\">")
+            val textWithBold = text
+                .replace("'", " ")
+                .replace("\n", "<br>")
+                .replace(
+                    "(?i)$term".toRegex(),
+                    "<b>" + term!!.uppercase(Locale.getDefault()) + "</b>"
+                )
+            builder.append(textWithBold)
+            builder.append("</font></div></html>")
+            return builder.toString()
+        }
+    }
+}
