a5d14ef2d81a1b8bd3064a43db87d8387ff266f0
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2023-04-17 18:26:53 -0300

    Extraccion getDBInWriteMode y getDBInReadMode

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
@@ -33,7 +33,7 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
             }
         }
         private fun getResultSet(statement: Statement) = statement.executeQuery("select * from artists")
-        private fun readResultSet(rs:ResultSet){//Este metodo seria mas correcto rs.readResultSet?
+        private fun readResultSet(rs:ResultSet){//Este metodo seria mas correcto rs.readResultSet? Habria que hacer un read para cada columna?
             while (rs.next()) {
                 println("id = " + rs.getInt("id"))
                 println("artist = " + rs.getString("artist"))
@@ -49,38 +49,40 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
             return statement
         }
 
+        private fun getDBInWriteMode(db: DataBase) = db.writableDatabase
+        private fun getDBInReadMode(db: DataBase) = db.readableDatabase
+
         @JvmStatic
         fun saveArtist(dbHelper: DataBase, artist: String?, info: String?) {
-            // Gets the data repository in write mode
-            val db = dbHelper.writableDatabase
+            val db = getDBInWriteMode(dbHelper)
 
-// Create a new map of values, where column names are the keys
+            // Create a new map of values, where column names are the keys
             val values = ContentValues()
             values.put("artist", artist)
             values.put("info", info)
             values.put("source", 1)
 
-// Insert the new row, returning the primary key value of the new row
+            // Insert the new row, returning the primary key value of the new row
             val newRowId = db.insert("artists", null, values)
         }
 
         @JvmStatic
         fun getInfo(dbHelper: DataBase, artist: String): String? {
-            val db = dbHelper.readableDatabase
+            val db = getDBInReadMode(dbHelper)
 
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
+            // Define a projection that specifies which columns from the database
+            // you will actually use after this query.
             val projection = arrayOf(
                 "id",
                 "artist",
                 "info"
             )
 
-// Filter results WHERE "title" = 'My Title'
+            // Filter results WHERE "title" = 'My Title'
             val selection = "artist  = ?"
             val selectionArgs = arrayOf(artist)
 
-// How you want the results sorted in the resulting Cursor
+            // How you want the results sorted in the resulting Cursor
             val sortOrder = "artist DESC"
             val cursor = db.query(
                 "artists",  // The table to query
