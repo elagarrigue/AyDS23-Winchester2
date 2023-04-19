28aa7e4f9becab7f188309775724c2113f612dbf
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2023-04-19 10:30:37 -0300

    extraccion contentValues y sortOrder

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
@@ -15,27 +15,31 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
         private const val COLUMN_SOURCE = "source"
         private const val TABLE_NAME = "artists"
         private const val SELECTION = "$COLUMN_ARTIST = ?"
+        private const val SORT_ORDER_CURSOR = "$COLUMN_ARTIST DESC"
         
         @JvmStatic
         fun saveArtist(dbHelper: DataBase, artist: String?, info: String?) {
             val db = getDBInWriteMode(dbHelper)
 
             // Create a new map of values, where column names are the keys
+            val values = contentValues(artist, info)
+
+            // Insert the new row, returning the primary key value of the new row
+            db.insert(TABLE_NAME, null, values)
+        }
+
+        private fun contentValues(artist: String?, info: String?): ContentValues {
             val values = ContentValues()
             values.put(COLUMN_ARTIST, artist)
             values.put(COLUMN_INFO, info)
             values.put(COLUMN_SOURCE, 1)
-
-            // Insert the new row, returning the primary key value of the new row
-            db.insert(TABLE_NAME, null, values)
+            return values
         }
 
         @JvmStatic
         fun getInfo(dbHelper: DataBase, artist: String): String? {
             val db = getDBInReadMode(dbHelper)
 
-            // How you want the results sorted in the resulting Cursor
-            val sortOrder = "$COLUMN_ARTIST DESC"
             val cursor = db.query(
                 TABLE_NAME,
                 columnsToReturn(),
@@ -43,7 +47,7 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
                 selectionArgs(artist),  // The values for the WHERE clause
                 null,  // don't group the rows
                 null,  // don't filter by row groups
-                sortOrder
+                SORT_ORDER_CURSOR
             )
             val items: MutableList<String> = ArrayList()
             while (cursor.moveToNext()) {
