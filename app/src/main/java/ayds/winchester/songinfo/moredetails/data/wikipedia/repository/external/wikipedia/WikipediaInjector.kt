3863dc2f040eb0ecf5b6e57f46b672307982f14e
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2023-05-06 15:22:48 -0300

    InfoRepository + WikipediaInjector

---
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/external/wikipedia/WikipediaInjector.kt
@@ -0,0 +1,7 @@
+package ayds.winchester.songinfo.moredetails.data.wikipedia.repository.external.wikipedia
+
+import ayds.winchester.songinfo.moredetails.data.wikipedia.repository.external.wikipedia.tracks.*
+
+object WikipediaInjector {
+    val wikipediaTrackService: WikipediaTrackService = WikipediaTrackInjector.wikipediaTrackService
+}
