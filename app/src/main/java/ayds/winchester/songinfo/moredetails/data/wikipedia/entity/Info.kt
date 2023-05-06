a3c09524bc7a2ce9e6178750aebdd76162d7dc98
AldanaNonnenmacher <89547316+AldanaNonnenmacher@users.noreply.github.com>
2023-05-06 15:13:58 -0300

    Se extrajo id y name de la creacion del artista.

--- app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/entity/Info.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/entity/Info.kt
@@ -3,8 +3,6 @@ package ayds.winchester.songinfo.moredetails.data.wikipedia.entity
 sealed class Info {
 
     data class ArtistInfo(
-        var id: String,
-        var name: String,
         var description: String,
         var wikipediaURL: String,
         var isLocallyStored: Boolean = false,
