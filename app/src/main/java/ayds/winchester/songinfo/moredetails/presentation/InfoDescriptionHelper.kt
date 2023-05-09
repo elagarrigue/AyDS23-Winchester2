397b82fad804ad9f549c75453b933ee2fda1d0ff
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-05-09 13:25:08 -0300

    Refactor + isLocallyStored added to UIState

--- app/src/main/java/ayds/winchester/songinfo/moredetails/presentation/InfoDescriptionHelper.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/presentation/InfoDescriptionHelper.kt
@@ -1,25 +1,16 @@
 package ayds.winchester.songinfo.moredetails.presentation
 
-import ayds.winchester.songinfo.moredetails.domain.entity.Info
-import ayds.winchester.songinfo.moredetails.domain.entity.Info.ArtistInfo
-import ayds.winchester.songinfo.moredetails.domain.entity.Info.EmptyInfo
-
 interface InfoDescriptionHelper {
-    fun getInfoDescriptionText(info: Info = EmptyInfo, artistName: String = ""): String
+    fun getInfoDescriptionText(artistState: MoreDetailsUiState, artistName: String = ""): String
 }
 
 internal class InfoDescriptionHelperImpl(private val htmlFormatter: HtmlFormatter): InfoDescriptionHelper {
-    override fun getInfoDescriptionText(info: Info, artistName: String): String {
-        return when (info) {
-            is ArtistInfo ->
-                "${
-                    if (info.isLocallyStored) "[*]" else ""
-                }${info.getFormattedDescription(artistName)}"
-            else -> "No Results"
-        }
-    }
+    override fun getInfoDescriptionText(artistState: MoreDetailsUiState, artistName: String) =
+        "${
+            if (artistState.isLocallyStored) "[*]" else ""
+        }${artistState.getFormattedDescription(artistName)}"
 
-    private fun ArtistInfo.getFormattedDescription(artistName: String) =
-        htmlFormatter.reformatToHtml(this.description, artistName)
+    private fun MoreDetailsUiState.getFormattedDescription(artistName: String) =
+        htmlFormatter.reformatToHtml(this.artistInfoDescription, artistName)
 
 }
--- app/src/main/java/ayds/winchester/songinfo/moredetails/presentation/MoreDetailsUiState.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/presentation/MoreDetailsUiState.kt
@@ -3,7 +3,8 @@ package ayds.winchester.songinfo.moredetails.presentation
 data class MoreDetailsUiState(
     val wikipediaDefaultImage: String = DEFAULT_IMAGE,
     val artistInfoDescription: String = "",
-    val artistInfoUrl: String = ""
+    val artistInfoUrl: String = "",
+    val isLocallyStored: Boolean = false
 ){
     companion object {
         const val DEFAULT_IMAGE =
