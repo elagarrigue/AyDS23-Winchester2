ed73d1bb2c467c36bc4b121b28922a06fec2cfc9
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-05-21 01:17:15 -0300

    Placeholder para indicador de la fuente de inf

--- app/src/main/java/ayds/winchester/songinfo/moredetails/presentation/MoreDetailsView.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/presentation/MoreDetailsView.kt
@@ -3,6 +3,7 @@ package ayds.winchester.songinfo.moredetails.presentation
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
@@ -24,6 +25,9 @@ class MoreDetailsViewImpl: AppCompatActivity(), MoreDetailsView{
     private lateinit var openUrlButton: Button
     private lateinit var imageView: ImageView
     private lateinit var moreDetailsPresenter: MoreDetailsPresenter
+    private lateinit var sourceLabel: TextView
+    private lateinit var spinnerUI: Spinner
+
     private val observer: Observer<MoreDetailsUiState> =
         Observer {
                 value -> updateView(value)
@@ -49,6 +53,8 @@ class MoreDetailsViewImpl: AppCompatActivity(), MoreDetailsView{
         artistInfoTextPane = findViewById(R.id.textPane2)
         openUrlButton = findViewById(R.id.openUrlButton)
         imageView = findViewById(R.id.imageView)
+        spinnerUI = findViewById(R.id.spinner)
+        sourceLabel = findViewById(R.id.sourceTextView)
     }
 
     private fun updateArtistInfo() {
