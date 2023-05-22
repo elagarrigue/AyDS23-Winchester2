c6e8384cd12128dca294f6a229db53adf8f2b2f3
AldanaNonnenmacher <89547316+AldanaNonnenmacher@users.noreply.github.com>
2023-05-22 11:11:00 -0300

    Implementacion del injector externo.

--- app/src/main/java/ayds/winchester/songinfo/moredetails/injector/MoreDetailsInjector.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/injector/MoreDetailsInjector.kt
@@ -2,38 +2,23 @@ package ayds.winchester.songinfo.moredetails.injector
 
 import android.content.Context
 import ayds.winchester.songinfo.moredetails.data.wikipedia.repository.WikipediaRepositoryImpl
-import ayds.winchester2.wikipediaexternal.data.wikipedia.WikipediaTrackService
-import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.JsonToInfoResolver
-import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.WikipediaTrackAPI
-import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.WikipediaTrackServiceImpl
-import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.WikipediaToInfoResolver
 import ayds.winchester.songinfo.moredetails.data.wikipedia.repository.local.wikipedia.WikipediaLocalStorage
 import ayds.winchester.songinfo.moredetails.data.wikipedia.repository.local.wikipedia.sqldb.CursorToWikipediaInfoMapper
 import ayds.winchester.songinfo.moredetails.data.wikipedia.repository.local.wikipedia.sqldb.CursorToWikipediaInfoMapperImpl
 import ayds.winchester.songinfo.moredetails.data.wikipedia.repository.local.wikipedia.sqldb.WikipediaLocalStorageImpl
 import ayds.winchester.songinfo.moredetails.domain.repository.WikipediaRepository
 import ayds.winchester.songinfo.moredetails.presentation.*
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import ayds.winchester2.wikipediaexternal.injector.WikipediaInjector
 
-private const val WIKIPEDIA_URL = "https://en.wikipedia.org/w/"
 
 object MoreDetailsInjector {
-
-    private val wikipediaAPIRetrofit = getRetrofit()
-    private val wikipediaTrackAPI = getWikipediaAPI(wikipediaAPIRetrofit)
-    private val wikipediaToInfoResolver: WikipediaToInfoResolver = JsonToInfoResolver()
-    private val wikipediaTrackService: WikipediaTrackService =
-        WikipediaTrackServiceImpl(
-            wikipediaTrackAPI,
-            wikipediaToInfoResolver
-        )
 
     private val descriptionFormatter: DescriptionFormatter = HtmlDescriptionFormatter()
     private val infoDescriptionHelper: InfoDescriptionHelper = InfoDescriptionHelperImpl(descriptionFormatter)
 
     private val cursorToWikipediaInfoMapper: CursorToWikipediaInfoMapper = CursorToWikipediaInfoMapperImpl()
     private lateinit var wikipediaLocalStorage: WikipediaLocalStorage
+    private val wikipediaTrackService = WikipediaInjector.wikipediaTrackService
 
     private lateinit var repository: WikipediaRepository
     private lateinit var moreDetailsPresenter: MoreDetailsPresenter
@@ -56,11 +41,4 @@ object MoreDetailsInjector {
     private fun initRepository(){
         repository = WikipediaRepositoryImpl(wikipediaLocalStorage, wikipediaTrackService)
     }
-
-    private fun getRetrofit() = Retrofit.Builder()
-        .baseUrl(WIKIPEDIA_URL)
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .build()
-
-    private fun getWikipediaAPI(retrofit: Retrofit) = retrofit.create(WikipediaTrackAPI::class.java)
 }
