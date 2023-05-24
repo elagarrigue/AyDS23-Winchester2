7b196e3e52efd24946a0f2c45a105165259b4895
AldanaNonnenmacher <89547316+AldanaNonnenmacher@users.noreply.github.com>
2023-05-23 21:04:34 -0300

    Broker + Proxy.

--- app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/broker/Broker.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/broker/Broker.kt
@@ -1,5 +1,13 @@
 package ayds.winchester.songinfo.moredetails.data.wikipedia.repository.broker
 
+import ayds.winchester.songinfo.moredetails.domain.entity.Card
+
 interface Broker {
+    fun getCard(artistName: String): Card.ArtistCard?
+}
+
+internal class ArtistCardBroker(private val wikipediaProxy: WikipediaProxy): Broker {
+    override fun getCard(artistName: String) =
+        wikipediaProxy.getCard(artistName)
 
 }
--- app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/broker/Proxy.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/broker/Proxy.kt
@@ -4,18 +4,20 @@ import ayds.winchester.songinfo.moredetails.domain.entity.Card
 import ayds.winchester.songinfo.moredetails.domain.entity.Card.ArtistCard
 import ayds.winchester.songinfo.moredetails.domain.entity.Source
 import ayds.winchester2.wikipediaexternal.data.wikipedia.WikipediaTrackService
+import ayds.winchester2.wikipediaexternal.data.wikipedia.entity.ArtistInfo
 
 interface Proxy {
     fun getCard(artistName: String): Card?
 }
-internal class WikipediaProxy(private val wikipediaTrackService: WikipediaTrackService): Proxy{
+internal class WikipediaProxy(private val wikipediaTrackService: WikipediaTrackService): Proxy {
     override fun getCard(artistName: String) =
-        wikipediaTrackService.getInfo(artistName)?.let {
-            ArtistCard(
-                description = it.description,
-                infoURL = it.wikipediaURL,
-                source = Source.Wikipedia,
-                sourceLogoUrl = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
-            )
-        }
+        wikipediaTrackService.getInfo(artistName)?.map()
+
+    private fun ArtistInfo.map() =
+        ArtistCard(
+            description = this.description,
+            infoURL = this.wikipediaURL,
+            source = Source.Wikipedia,
+            sourceLogoUrl = this.wikipediaLogoURL
+        )
 }
