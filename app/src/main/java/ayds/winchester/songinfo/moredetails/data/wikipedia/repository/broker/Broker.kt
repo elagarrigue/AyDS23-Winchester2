1f2fdf8f9d0d08ea4ad342ea3b697a2e6005fb8f
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2023-05-28 15:27:46 -0300

    Updated Broker for multiple proxys

--- app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/broker/Broker.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/data/wikipedia/repository/broker/Broker.kt
@@ -3,11 +3,15 @@ package ayds.winchester.songinfo.moredetails.data.wikipedia.repository.broker
 import ayds.winchester.songinfo.moredetails.domain.entity.Card
 
 interface Broker {
-    fun getCard(artistName: String): Card.ArtistCard?
+    fun getCards(artistName: String): List<Card>
 }
 
-internal class ArtistCardBroker(private val wikipediaProxy: WikipediaProxy): Broker {
-    override fun getCard(artistName: String) =
-        wikipediaProxy.getCard(artistName)
-
+internal class ArtistCardBroker(private val proxiesList: List<Proxy>): Broker {
+    override fun getCards(artistName: String): List<Card>{
+        val cardsList = mutableListOf<Card>()
+        for(proxy in proxiesList){
+            proxy.getCard(artistName)?.let { cardsList.add(it)}
+        }
+        return cardsList
+    }
 }
